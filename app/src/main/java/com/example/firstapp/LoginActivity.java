ecd51f2ca1b6210ee0ecb3594792ba91d206a86d
IvAnT <ant.ivan99@mail.ru>
2020-04-27 13:34:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/firstapp/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.example.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText login;
+    EditText pass;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login = (EditText) findViewById(R.id.login);
+        pass = (EditText) findViewById(R.id.pass);
+        button = (Button) findViewById(R.id.submit_auth);
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String loginText = login.getText().toString();
+                String passText = pass.getText().toString();
+                if (!passText.equals(getString(R.string.password))) {
+                    return;
+                }
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("login", loginText);
+                startActivity(intent);
+            }
+        };
+
+        button.setOnClickListener(onClickListener);
+    }
+}
---
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity
+{
+    private DrawerLayout mDrawerLayout;
+    private TextView login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        replaceTransaction(new ChooseCountryFragment());
+        login = findViewById(R.id.login);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String userName = extras.getString("login");
+            View head = navigationView.getHeaderView(0);
+            TextView text = head.findViewById(R.id.loginHead);
+            text.setText(userName);
+        }
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        //menuItem.setChecked(true);
+                        switch (menuItem.getItemId()) {
+                            case R.id.country_info_item:
+                                replaceTransaction(new CountryInfoFragment());
+                                break;
+                            case R.id.saved_countries_item:
+                                replaceTransaction(new SavedCountriesFragment());
+                                break;
+                            case R.id.choose_country_item:
+                                replaceTransaction(new ChooseCountryFragment());
+                                break;
+                            case R.id.about_item:
+                                replaceTransaction(new AboutFragment());
+                                break;
+                            default:
+                                return false;
+                        }
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+
+    }
+
+    private void replaceTransaction(Fragment newFragment)
+    {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.mainActivityFrame, newFragment)
+                .addToBackStack(null)
+                .commit();
+    }
+}
